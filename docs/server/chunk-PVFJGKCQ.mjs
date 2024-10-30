854bda5cd94445eb24c6b33b9ffa2aef9648dd9e
ankursharma <ansh2502@colorado.edu>
2024-10-30 13:50:27 -0400

    revert

---
+++ docs/server/chunk-PVFJGKCQ.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as r,b as p,c as n,d as i,e as m,j as a,m as f,o as s,p as c,s as l,y as d}from"./chunk-VIONULFM.mjs";var o=class e{title="temp-angular-project";static \u0275fac=function(t){return new(t||e)};static \u0275cmp=r({type:e,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(t,R){t&1&&(p(0,"head"),i(1,` Lets GO!
+`),n())}})};var g=[];var u={providers:[a({eventCoalescing:!0}),d(g),c()]};var A={providers:[l()]},C=f(u,A);var x=()=>s(o,C),k=x;export{k as a};
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DF3C7L2V.mjs";import"./chunk-GTLE74PO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-PVFJGKCQ.mjs";import"./chunk-VIONULFM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
